e169d3b0c8af7c029ff2bac814cecb4446c4a847
sourav <souravas007@gmail.com>
2021-02-16 15:47:27 +0530

    [array:medium] Added word-search.

---
+++ src/com/code/leetcode/backtracking/medium/WordSearch.java
@@ -0,0 +1,51 @@
+package com.code.leetcode.backtracking.medium;
+
+public class WordSearch {
+
+	/*
+	 * Question: https://leetcode.com/problems/word-search/
+	 * */
+	private char[][] board;
+	private int ROWS;
+	private int COLS;
+
+	public boolean exist( char[][] board, String word ) {
+		this.board = board;
+		this.ROWS = board.length;
+		this.COLS = board[0].length;
+
+		for ( int row = 0; row < this.ROWS; row++ ) {
+			for ( int col = 0; col < this.COLS; col++ ) {
+				if ( this.backtrack( row, col, word, 0 ) ) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean backtrack( int row, int col, String word, int index ) {
+		if ( index >= word.length() ) {
+			return true;
+		}
+		if ( row < 0 || row == this.ROWS || col < 0 || col == this.COLS
+				|| this.board[row][col] != word.charAt( index ) ) {
+			return false;
+		}
+		boolean ret = false;
+		this.board[row][col] = '#';
+
+		int[] rowOffsets = { 0, 1, 0, -1 };
+		int[] colOffsets = { 1, 0, -1, 0 };
+		for ( int d = 0; d < 4; d++ ) {
+			ret = this.backtrack( row + rowOffsets[d], col + colOffsets[d], word, index + 1 );
+			// why ret? so that the starting value will be changed to the character from word instead of '#'.
+			if ( ret ) {
+				break;
+			}
+		}
+		this.board[row][col] = word.charAt( index );
+		return ret;
+	}
+
+}
